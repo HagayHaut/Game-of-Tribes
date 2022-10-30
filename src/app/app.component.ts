545d3bcf400ceee87cd752bef6c737417d25044d
Hagay Haut <hagayhaut@gmail.com>
2022-10-29 21:32:35 -0400

    create static board with one-way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+const WIDTH = 40;
+const HEIGHT = 30;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'GoL-angular';
+  civ = Array(HEIGHT)
+    .fill([])
+    .map((_, r) => Array(WIDTH).fill(false));
 }
---
+++ src/app/cell/cell.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cell',
+  templateUrl: './cell.component.html',
+  styleUrls: ['./cell.component.css'],
+})
+export class CellComponent implements OnInit {
+  @Input() isAlive: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
