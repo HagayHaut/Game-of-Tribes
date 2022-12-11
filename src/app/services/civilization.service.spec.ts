fa5290c826baad5ab0757c1fd07095c550b97ef0
Hagay Haut <hagayhaut@gmail.com>
2022-12-10 21:56:24 -0500

    add unit tests for getNextGen()

--- src/app/services/civilization.service.spec.ts
+++ src/app/services/civilization.service.spec.ts
@@ -98,7 +98,7 @@ describe('CivilizationService', () => {
       expect(liveCellCount).toEqual(36);
     });
 
-    it('should place the boats in the corners, regardless of screen size', () => {
+    it('should place the boats in the corners, regardless of board size', () => {
       const randomFromRange = (min: number, max: number): number => {
         return ~~(Math.random() * (max - min) + min);
       };
@@ -147,7 +147,7 @@ describe('CivilizationService', () => {
       expect(liveCellCount).toEqual(20);
     });
 
-    it('should place the gliders in the corners, regardless of screen size', () => {
+    it('should place the gliders in the corners, regardless of board size', () => {
       const randomFromRange = (min: number, max: number): number => {
         return ~~(Math.random() * (max - min) + min);
       };
@@ -167,6 +167,196 @@ describe('CivilizationService', () => {
       expect(randomGliderCiv[0][randomWidth - 3]).toEqual(2);
       // bottom right
       expect(randomGliderCiv[randomHeight - 3][randomWidth - 3]).toEqual(1);
+    });
+  });
+
+  describe('getPowerColonyCiv(): ', () => {
+    let powerColonyCiv: number[][];
+
+    beforeEach(() => {
+      powerColonyCiv = civService.getPowerColonyCiv();
+    });
+
+    it('should return a 2-D array with initial width and height', () => {
+      expect(powerColonyCiv[0].length).toEqual(width);
+      expect(powerColonyCiv.length).toEqual(height);
+    });
+
+    it('should contain 7 live cells', () => {
+      let liveCellCount = 0;
+      powerColonyCiv.forEach((row) => {
+        row.forEach((cell) => cell && liveCellCount++);
+      });
+      expect(liveCellCount).toEqual(7);
+    });
+
+    it('should place the power colony in the center, regardless of board size', () => {
+      const randomFromRange = (min: number, max: number): number => {
+        return ~~(Math.random() * (max - min) + min);
+      };
+      const randomWidth = randomFromRange(50, 100);
+      const randomHeight = randomFromRange(40, 80);
+      const randomSizeCivService = new CivilizationService(
+        randomWidth,
+        randomHeight
+      );
+      const randomSizePowerColonyCiv = randomSizeCivService.getPowerColonyCiv();
+
+      const midWidth = ~~(randomWidth / 2);
+      const midHeight = ~~(randomHeight / 2);
+
+      expect(randomSizePowerColonyCiv[midHeight][midWidth]).toEqual(1);
+      expect(randomSizePowerColonyCiv[midHeight - 1][midWidth - 2]).toEqual(2);
+      expect(randomSizePowerColonyCiv[midHeight - 1][midWidth - 1]).toEqual(0);
+    });
+  });
+
+  describe('getNextGen(): [boolean, number[][]]', () => {
+    let firstGeneration: number[][];
+
+    beforeEach(() => {
+      firstGeneration = civService.getRandomCiv();
+    });
+
+    describe('first index of return value (boolean)', () => {
+      it('should be `true` when next gen is different from the input civ', () => {
+        const [changed, _] = civService.getNextGen(civService.getBoatCiv());
+        expect(changed).toEqual(true);
+      });
+
+      it('should be `false` when next gen is identical to the input civ', () => {
+        const [changed, _] = civService.getNextGen(civService.getInitialCiv());
+        expect(changed).toEqual(false);
+      });
+    });
+
+    describe('second index of return value (number[][])', () => {
+      it('should be a new gen of the same proportion as the input civ', () => {
+        const [_, secondGeneration] = civService.getNextGen(firstGeneration);
+        expect(secondGeneration[0].length).toEqual(width);
+        expect(secondGeneration.length).toEqual(height);
+      });
+    });
+
+    describe('A dead cell: ', () => {
+      let initialCiv: number[][];
+
+      beforeEach(() => {
+        initialCiv = Array(4)
+          .fill([])
+          .map((_) => Array(4).fill(0));
+
+        initialCiv[0][1] = 1;
+        initialCiv[1][0] = 1;
+        initialCiv[1][1] = 2;
+
+        const [_, nextGenCiv] = civService.getNextGen(initialCiv);
+        expect(nextGenCiv[0][0]).toEqual(1);
+      });
+
+      it('should come to life if it has exactly 3 live neighbors', () => {
+        const [_, nextGenCiv] = civService.getNextGen(initialCiv);
+
+        expect(nextGenCiv[0][0]).toBeTruthy();
+      });
+
+      it('should come to life with the tribe of the majority neighbor', () => {
+        const [_, nextGenCiv] = civService.getNextGen(initialCiv);
+
+        expect(nextGenCiv[0][0]).toEqual(1);
+      });
+
+      it('should remain dead if it has less than 3 live neighbors', () => {
+        const [_, nextGenCiv] = civService.getNextGen(initialCiv);
+
+        // 0 live neighbors
+        expect(nextGenCiv[3][3]).toEqual(0);
+        // 1 live neighbor
+        expect(nextGenCiv[0][2]).toEqual(0);
+      });
+
+      it('should remain dead if it has more than 3 live neighbors', () => {
+        const fullCiv: number[][] = Array(4)
+          .fill([])
+          .map((_) => Array(4).fill(1));
+
+        fullCiv[1][1] = 0;
+
+        const [_, nextGenCiv] = civService.getNextGen(fullCiv);
+
+        expect(nextGenCiv[1][1]).toEqual(0);
+      });
+    });
+
+    describe('A live cell: ', () => {
+      let deadCiv: number[][];
+
+      beforeEach(() => {
+        deadCiv = Array(4)
+          .fill([])
+          .map((_) => Array(4).fill(0));
+      });
+
+      it('should die if has less than two live neighbors', () => {
+        deadCiv[0][1] = 1;
+        deadCiv[1][1] = 1;
+        deadCiv[3][3] = 1;
+        const [_, nextGenCiv] = civService.getNextGen(deadCiv);
+        // 1 live neighbor
+        expect(nextGenCiv[0][1]).toEqual(0);
+        // 0 live neighbors
+        expect(nextGenCiv[3][3]).toEqual(0);
+      });
+
+      it('should die if it has more than three live neighbors', () => {
+        const liveCiv: number[][] = deadCiv.map((row) => row.map((_) => 1));
+
+        const [_, nextGenCiv] = civService.getNextGen(liveCiv);
+
+        expect(nextGenCiv[0][1]).toEqual(0);
+        expect(nextGenCiv[1][1]).toEqual(0);
+      });
+
+      it('should remain the same tribe if out of two live neighbors one is the same tribe as itself', () => {
+        deadCiv[0][0] = 1;
+        deadCiv[0][1] = 1;
+        deadCiv[0][2] = 2;
+
+        const [_, nextGenCiv] = civService.getNextGen(deadCiv);
+        expect(nextGenCiv[1][1]).toEqual(1);
+      });
+
+      it('should remain the same tribe if it has two live neighbors of the same tribe as itself', () => {
+        deadCiv[0][0] = 1;
+        deadCiv[0][1] = 1;
+        deadCiv[0][2] = 1;
+
+        const [_, nextGenCiv] = civService.getNextGen(deadCiv);
+        expect(nextGenCiv[1][1]).toEqual(1);
+      });
+
+      it('should switch tribes if it has two live neighbors of a different tribe', () => {
+        deadCiv[0][0] = 1;
+        deadCiv[0][1] = 2;
+        deadCiv[0][2] = 1;
+
+        const [_, nextGenCiv] = civService.getNextGen(deadCiv);
+        expect(nextGenCiv[1][1]).toEqual(1);
+      });
+
+      it('should become the tribe of the majority of its neighbors if it has three neighbors', () => {
+        deadCiv[0][1] = 2;
+        deadCiv[1][1] = 1;
+        deadCiv[2][1] = 2;
+        deadCiv[1][0] = 1;
+        deadCiv[1][2] = 2;
+
+        const [_, nextGenCiv] = civService.getNextGen(deadCiv);
+
+        expect(nextGenCiv[1][0]).toEqual(2);
+        expect(nextGenCiv[0][1]).toEqual(1);
+        expect(nextGenCiv[1][2]).toEqual(2);
+      });
     });
   });
 });
