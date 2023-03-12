3b45cf676176d59ab394d9e4a51efe682459f9c2
Hagay Haut <hagayhaut@gmail.com>
2023-03-11 23:55:54 -0500

    wip: refactor civ service type system

--- src/app/models/app.states.ts
+++ src/app/models/app.states.ts
@@ -4,8 +4,17 @@ type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] exte
 
 type Range<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>;
 
-export type Cell = 0 | 1 | 2;
-export type Civilization = Cell[][];
 export type Resolution = Range<3, 8>;
+
 export type Coordinate = Range<0, 100>;
+
 export type Coordinates = [Coordinate, Coordinate];
+
+export type CellState =  0 | 1 | 2;
+
+export interface Cell {
+  state: CellState;
+  coors: Coordinates;
+};
+
+export type Civilization = Cell[][];
--- src/app/services/civilization.service.ts
+++ src/app/services/civilization.service.ts
@@ -1,41 +1,47 @@
-import { Cell, Civilization } from "../models/app.states";
+import { Cell, CellState, Civilization, Coordinate } from "../models/app.states";
 
 export class CivilizationService {
-  public width: number;
-  public height: number;
+  public width: Coordinate;
+  public height: Coordinate;
 
-  constructor(width: number, height: number) {
+  constructor(width: Coordinate, height: Coordinate) {
     this.width = width;
     this.height = height;
   }
 
   getInitialCiv({ width, height } = this): Civilization {
-    return Array(height)
-      .fill([])
-      .map((_) => Array(width).fill(0));
+    const civ: Civilization = [];
+
+    for (let r = 0; r < height; r++) {
+      const row: Cell[] = [];
+      for (let c = 0; c < width; c++) {
+        row.push({ state: 0, coors: [r as Coordinate, c as Coordinate]})
+      }
+      civ.push(row);
+    }
+    return civ;
   }
 
   getRandomCiv({ width, height } = this): Civilization {
-    return Array(height)
-      .fill([])
-      .map(() =>
-        Array(width)
-          .fill(0)
-          .map(() => {
-            const rand = Math.floor(Math.random() * 8);
-            return [1, 2].includes(rand) ? rand : 0;
-          })
-      ) as Civilization;
+
+    const civ: Civilization = [];
+
+    for (let r = 0; r < height; r++) {
+      const row: Cell[] = [];
+      for (let c = 0; c < width; c++) {
+        const rand = Math.floor(Math.random() * 8);
+        row.push({ 
+          state: [1, 2].includes(rand) ? rand as CellState : 0, 
+          coors: [r as Coordinate, c as Coordinate]
+        });
+      }
+      civ.push(row);
+    }
+    return civ;
   }
 
-  getBoatCiv({ width, height } = this): Civilization {
-    const boatCiv: Civilization = Array(height)
-      .fill([])
-      .map(() =>
-        Array(width)
-          .fill(0)
-          .map(() => 0)
-      );
+  getBoatCiv({ width, height, getInitialCiv } = this): Civilization {
+    const boatCiv = getInitialCiv();
 
     // make anchors
 
@@ -50,111 +56,99 @@ export class CivilizationService {
     const [h4, w4] = [height - 6, width - 6];
 
     // top left
-    boatCiv[h1][w1] = 1;
-    boatCiv[h1 + 1][w1] = 1;
-    boatCiv[h1 + 2][w1] = 1;
-    boatCiv[h1 - 1][w1 + 1] = 1;
-    boatCiv[h1 + 2][w1 + 1] = 1;
-    boatCiv[h1 + 2][w1 + 2] = 1;
-    boatCiv[h1 + 2][w1 + 3] = 1;
-    boatCiv[h1 + 1][w1 + 4] = 1;
-    boatCiv[h1 - 1][w1 + 4] = 1;
+    boatCiv[h1][w1].state = 1;
+    boatCiv[h1 + 1][w1].state = 1;
+    boatCiv[h1 + 2][w1].state = 1;
+    boatCiv[h1 - 1][w1 + 1].state = 1;
+    boatCiv[h1 + 2][w1 + 1].state = 1;
+    boatCiv[h1 + 2][w1 + 2].state = 1;
+    boatCiv[h1 + 2][w1 + 3].state = 1;
+    boatCiv[h1 + 1][w1 + 4].state = 1;
+    boatCiv[h1 - 1][w1 + 4].state = 1;
 
     // bottom left
-    boatCiv[h2][w2] = 2;
-    boatCiv[h2 - 1][w2] = 2;
-    boatCiv[h2 - 2][w2] = 2;
-    boatCiv[h2 + 1][w2 + 1] = 2;
-    boatCiv[h2 - 2][w2 + 1] = 2;
-    boatCiv[h2 - 2][w2 + 2] = 2;
-    boatCiv[h2 - 2][w2 + 3] = 2;
-    boatCiv[h2 + 1][w2 + 4] = 2;
-    boatCiv[h2 - 1][w2 + 4] = 2;
+    boatCiv[h2][w2].state = 2;
+    boatCiv[h2 - 1][w2].state = 2;
+    boatCiv[h2 - 2][w2].state = 2;
+    boatCiv[h2 + 1][w2 + 1].state = 2;
+    boatCiv[h2 - 2][w2 + 1].state = 2;
+    boatCiv[h2 - 2][w2 + 2].state = 2;
+    boatCiv[h2 - 2][w2 + 3].state = 2;
+    boatCiv[h2 + 1][w2 + 4].state = 2;
+    boatCiv[h2 - 1][w2 + 4].state = 2;
 
     // top right
-    boatCiv[h3][w3] = 2;
-    boatCiv[h3 + 1][w3] = 2;
-    boatCiv[h3 + 2][w3] = 2;
-    boatCiv[h3 - 1][w3 - 1] = 2;
-    boatCiv[h3 + 2][w3 - 1] = 2;
-    boatCiv[h3 + 2][w3 - 2] = 2;
-    boatCiv[h3 + 2][w3 - 3] = 2;
-    boatCiv[h3 + 1][w3 - 4] = 2;
-    boatCiv[h3 - 1][w3 - 4] = 2;
+    boatCiv[h3][w3].state = 2;
+    boatCiv[h3 + 1][w3].state = 2;
+    boatCiv[h3 + 2][w3].state = 2;
+    boatCiv[h3 - 1][w3 - 1].state = 2;
+    boatCiv[h3 + 2][w3 - 1].state = 2;
+    boatCiv[h3 + 2][w3 - 2].state = 2;
+    boatCiv[h3 + 2][w3 - 3].state = 2;
+    boatCiv[h3 + 1][w3 - 4].state = 2;
+    boatCiv[h3 - 1][w3 - 4].state = 2;
 
     // bottom right
-    boatCiv[h4][w4] = 1;
-    boatCiv[h4 - 1][w4] = 1;
-    boatCiv[h4 - 2][w4] = 1;
-    boatCiv[h4 + 1][w4 - 1] = 1;
-    boatCiv[h4 - 2][w4 - 1] = 1;
-    boatCiv[h4 - 2][w4 - 2] = 1;
-    boatCiv[h4 - 2][w4 - 3] = 1;
-    boatCiv[h4 + 1][w4 - 4] = 1;
-    boatCiv[h4 - 1][w4 - 4] = 1;
+    boatCiv[h4][w4].state = 1;
+    boatCiv[h4 - 1][w4].state = 1;
+    boatCiv[h4 - 2][w4].state = 1;
+    boatCiv[h4 + 1][w4 - 1].state = 1;
+    boatCiv[h4 - 2][w4 - 1].state = 1;
+    boatCiv[h4 - 2][w4 - 2].state = 1;
+    boatCiv[h4 - 2][w4 - 3].state = 1;
+    boatCiv[h4 + 1][w4 - 4].state = 1;
+    boatCiv[h4 - 1][w4 - 4].state = 1;
 
     return boatCiv;
   }
 
-  getGliderCiv({ width, height } = this): Civilization {
-    const gliderCiv: Civilization = Array(height)
-      .fill([])
-      .map(() =>
-        Array(width)
-          .fill(0)
-          .map(() => 0)
-      );
+  getGliderCiv({ width, height, getInitialCiv } = this): Civilization {
+    const gliderCiv = getInitialCiv();
 
     // top left
-    gliderCiv[0][1] = 1;
-    gliderCiv[1][2] = 1;
-    gliderCiv[2][0] = 1;
-    gliderCiv[2][1] = 1;
-    gliderCiv[2][2] = 1;
+    gliderCiv[0][1].state = 1;
+    gliderCiv[1][2].state = 1;
+    gliderCiv[2][0].state = 1;
+    gliderCiv[2][1].state = 1;
+    gliderCiv[2][2].state = 1;
 
     // bottom left
-    gliderCiv[height - 2][0] = 2;
-    gliderCiv[height - 3][1] = 2;
-    gliderCiv[height - 3][2] = 2;
-    gliderCiv[height - 2][2] = 2;
-    gliderCiv[height - 1][2] = 2;
+    gliderCiv[height - 2][0].state = 2;
+    gliderCiv[height - 3][1].state = 2;
+    gliderCiv[height - 3][2].state = 2;
+    gliderCiv[height - 2][2].state = 2;
+    gliderCiv[height - 1][2].state = 2;
 
     // top right
-    gliderCiv[0][width - 3] = 2;
-    gliderCiv[1][width - 3] = 2;
-    gliderCiv[2][width - 3] = 2;
-    gliderCiv[2][width - 2] = 2;
-    gliderCiv[1][width - 1] = 2;
+    gliderCiv[0][width - 3].state = 2;
+    gliderCiv[1][width - 3].state = 2;
+    gliderCiv[2][width - 3].state = 2;
+    gliderCiv[2][width - 2].state = 2;
+    gliderCiv[1][width - 1].state = 2;
 
     // bottom right
-    gliderCiv[height - 3][width - 3] = 1;
-    gliderCiv[height - 3][width - 2] = 1;
-    gliderCiv[height - 3][width - 1] = 1;
-    gliderCiv[height - 2][width - 3] = 1;
-    gliderCiv[height - 1][width - 2] = 1;
+    gliderCiv[height - 3][width - 3].state = 1;
+    gliderCiv[height - 3][width - 2].state = 1;
+    gliderCiv[height - 3][width - 1].state = 1;
+    gliderCiv[height - 2][width - 3].state = 1;
+    gliderCiv[height - 1][width - 2].state = 1;
 
     return gliderCiv;
   }
 
-  getPowerColonyCiv({ width, height } = this): Civilization {
-    const powerColony: Civilization = Array(height)
-      .fill([])
-      .map(() =>
-        Array(width)
-          .fill(0)
-          .map(() => 0)
-      );
+  getPowerColonyCiv({ width, height, getInitialCiv } = this): Civilization {
+    const powerColony = getInitialCiv();
 
     const midW = ~~(width / 2);
     const midH = ~~(height / 2);
 
-    powerColony[midH][midW] = 1;
-    powerColony[midH - 1][midW - 2] = 2;
-    powerColony[midH + 1][midW - 2] = 1;
-    powerColony[midH + 1][midW - 3] = 2;
-    powerColony[midH + 1][midW + 1] = 1;
-    powerColony[midH + 1][midW + 2] = 2;
-    powerColony[midH + 1][midW + 3] = 1;
+    powerColony[midH][midW].state = 1;
+    powerColony[midH - 1][midW - 2].state = 2;
+    powerColony[midH + 1][midW - 2].state = 1;
+    powerColony[midH + 1][midW - 3].state = 2;
+    powerColony[midH + 1][midW + 1].state = 1;
+    powerColony[midH + 1][midW + 2].state = 2;
+    powerColony[midH + 1][midW + 3].state = 1;
 
     return powerColony;
   }
