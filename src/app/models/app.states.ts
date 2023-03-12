b0f65aadd073a52f59bd191ca011a7db364a6311
Hagay Haut <hagayhaut@gmail.com>
2023-03-12 17:43:41 -0400

    restructure: type files

--- src/app/models/app.states.ts
+++ src/app/models/app.states.ts
@@ -1,8 +1,4 @@
-type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
-  ? Acc[number]
-  : Enumerate<N, [...Acc, Acc['length']]>;
-
-type Range<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>;
+import { Range } from "./app.util-types";
 
 export type Resolution = Range<3, 8>;
 
